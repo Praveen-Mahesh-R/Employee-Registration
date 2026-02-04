d42348bba33b4a9b459092b1dbf0a2f7b20e5ed9
praveen <praveen.adyar@gmail.com>
2026-02-04 19:19:20 +0530

    Adding html for site

--- emp_reg/models.py
+++ emp_reg/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
+    joindate = models.DateTimeField(default=timezone.now)
 
     
 # Create your models here.
--- emp_reg/views.py
+++ emp_reg/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def emp_list(request):
+    return render(request, 'emp_reg/emp_list.html', {})
