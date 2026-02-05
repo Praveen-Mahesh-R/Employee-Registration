4f809b3eada74e99786f5ec73f640db4a93acc42
praveen <praveen.adyar@gmail.com>
2026-02-05 10:10:51 +0530

    added join date in models

--- emp_reg/models.py
+++ emp_reg/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-from django.utils import timezone
+import datetime
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    gender = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
-    joindate = models.DateTimeField(default=timezone.now)
+    join_date = models.DateField(default=datetime.date.today)
+
+    def re(self):
+        self.join_date = datetime.date.today()
+        self.save()
+    def __str__(self):
+        return self.first_name
 
     
 # Create your models here.
