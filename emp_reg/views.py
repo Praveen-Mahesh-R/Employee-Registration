9053a76731bc0d89e83b397e32d0cede4e658a07
praveen <praveen.adyar@gmail.com>
2026-02-05 12:08:54 +0530

    modified templates to display employee data from database

--- emp_reg/views.py
+++ emp_reg/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Employee
+import datetime
 
 # Create your views here.
 def emp_list(request):
-    return render(request, 'emp_reg/emp_list.html', {})
+    emp = Employee.objects.order_by('join_date')
+    return render(request, 'emp_reg/emp_list.html', {'emp' : emp})
