94407606b077aa168ab0ef876a524f10fad2599e
praveen <praveen.adyar@gmail.com>
2026-02-18 21:10:29 +0530

    Added login menu and logout option

--- emp_reg/urls.py
+++ emp_reg/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+
+    
+    path("accounts/", include("django.contrib.auth.urls")),
+
     path('', views.emp_list, name='emp_list'),
-    #path('emp/search',views.emp_search, name='emp_search'),
-    #path('emp/del_search',views.emp_del_search, name='emp_del_search'),
     path('emp/new',views.emp_new, name='emp_new'),
     path('emp/deleted-record',views.emp_del_list, name='emp_del_list'),
     path('emp/<int:pk>/edit', views.emp_edit, name='emp_edit'),
@@ -13,4 +16,7 @@ urlpatterns = [
     path('emp/<int:pk>/restore', views.emp_restore, name='emp_restore'),
     path('emp/<int:pk>/rest_conf', views.emp_rest_conf, name='emp_rest_conf'),
     path('ajax/load-roles/', views.load_roles, name='ajax_load_roles'), 
+
+    #path('emp/search',views.emp_search, name='emp_search'),
+    #path('emp/del_search',views.emp_del_search, name='emp_del_search'),
 ]
--- employee/settings.py
+++ employee/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'emp_reg',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'employee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -148,3 +149,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "emp_list"
+
+LOGOUT_REDIRECT_URL = "emp_list"
